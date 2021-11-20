45a4ac9c5f63640cdad89c80f8e713dca27a3dda
Rajiv Sadho <rajsadho@users.noreply.github.com>
2021-11-20 12:11:27 -0400

    Fixed incorrect spelling

--- App/models/fruit_tag.py
+++ App/models/fruit_tag.py
@@ -3,10 +3,10 @@ from App.models import db
 
 class FruitTag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fruit_id = db.Colum(db.Integer, db.ForeignKey('fruit.id'), primary_key=True)
-    tag_id = db.Colum(db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+    fruit_id = db.Column(db.Integer, db.ForeignKey('fruit.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'), primary_key=True)
 
-    extra_data = db.Colum(db.String(50))
+    extra_data = db.Column(db.String(50))
 
     fruit = db.relationship('Fruit', backref='tags')
     tag =db. relationship('Tag', backref='fruits')
