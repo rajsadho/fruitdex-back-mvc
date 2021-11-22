9f63ab63d5b195d7c36bb7bd54c44616e14435a8
tariqm14 <tariqmohammed14@gmail.com>
2021-11-22 14:52:38 +0000

    updated tag controller

--- App/controllers/tag.py
+++ App/controllers/tag.py
@@ -9,3 +9,19 @@ def get_all_tags_json():
     tags = [tag.toDict() for tag in tags]
     return tags
 
+def create_tag(value, tag_type, description):
+    tag = tag(value=value, tag_type=tag_type, description=description)
+    db.session.add(tag)
+    db.session.commit()
+
+def create_tags(tags):
+    for tag in tags:
+        newTag = tag(value=tag['value'], tag_type=tag['tag_type'], description=tag['description'])
+        db.session.add(newTag)
+    db.session.commit()
+
+def get_tag_by_id(id):
+    return Tag.query.get(id)
+
+def get_tag_by_value(value):
+    return Tag.query.filter_by(value=value).first()
