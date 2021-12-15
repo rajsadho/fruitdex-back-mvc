2dbcf04b3f431ef75d5197079976bc58164c4bbb
Rajiv Sadho <rajsadho@users.noreply.github.com>
2021-12-15 19:18:00 -0400

    Edited fruit names controller

--- App/controllers/fruit_name.py
+++ App/controllers/fruit_name.py
@@ -1,27 +1,27 @@
-from flask import redirect, render_template, request, url_for, jsonify
+from flask import jsonify
 
-from App.models import ( fruit_name, db )
+from App.models import ( FruitName, db )
 
 def get_all_fruit_names_json():
-    fruits=fruit_name.query.all()
-    if not fruits:
-        return[]
-    fruits=[fruit.toDict()for fruit in fruits]
-    return fruits
+    names=FruitName.query.all()
+    if not names:
+        return jsonify(names=[])
+    names=[name.toDict() for name in names]
+    return jsonify(fruit_names=names)
 
 def create_fruit_name(name,name_type):
-    fruit=fruit_name(name=name,name_type=name_type)
+    fruit=FruitName(name=name,name_type=name_type)
     db.session.add(fruit)
     db.session.commit()
 
 def create_fruit_names(fruits):
     for fruit in fruits:
-        newfruit=fruit_name(name=fruit['name'],name_type=fruit['name_type'])
+        newfruit=FruitName(name=fruit['name'],name_type=fruit['name_type'])
         db.session.add(newfruit)
     db.session.commit()
 
 def get_fruit_by_name(name):
-    return fruit_name.query.filter_by(name=name).first()
+    return FruitName.query.filter_by(name=name).first()
 
 def get_fruit_by_nametype(name_type):
-    return fruit_name.query.filter_by(name_type=name_type).first()
+    return FruitName.query.filter_by(name_type=name_type).first()
