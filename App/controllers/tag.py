8a200af84157c83856dab8c1fe24e214e45537d2
Rajiv Sadho <rajsadho@users.noreply.github.com>
2021-12-15 19:19:46 -0400

    Edited tag controller

--- App/controllers/tag.py
+++ App/controllers/tag.py
@@ -5,18 +5,18 @@ from App.models import ( Tag, db )
 def get_all_tags_json():
     tags = Tag.query.all()
     if not tags:
-        return[]
+        return jsonify(tags=[])
     tags = [tag.toDict() for tag in tags]
-    return tags
+    return jsonify(tags=tags)
 
 def create_tag(value, tag_type, description):
-    tag = tag(value=value, tag_type=tag_type, description=description)
+    tag = Tag(value=value, tag_type=tag_type, description=description)
     db.session.add(tag)
     db.session.commit()
 
 def create_tags(tags):
     for tag in tags:
-        newTag = tag(value=tag['value'], tag_type=tag['tag_type'], description=tag['description'])
+        newTag = Tag(value=tag['value'], tag_type=tag['tag_type'], description=tag['description'])
         db.session.add(newTag)
     db.session.commit()
 
