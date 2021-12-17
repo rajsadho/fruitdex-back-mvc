cdf84ddb14ea0c69e3aaa846731f4d25396001bc
Rajiv Sadho <rajsadho@users.noreply.github.com>
2021-12-17 18:41:49 -0400

    Simplified models

--- App/models/fruit_tag.py
+++ App/models/fruit_tag.py
@@ -6,5 +6,5 @@ class FruitTag(db.Model):
     fruit_id = db.Column(db.Integer, db.ForeignKey('fruit.id'), primary_key=True)
     tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'), primary_key=True)
 
-    fruit = db.relationship('Fruit', backref='tag')
-    tag =db.relationship('Tag', backref='fruit')
+    fruit = db.relationship('Fruit', back_populates='tags')
+    tag =db.relationship('Tag', back_populates='fruits')
